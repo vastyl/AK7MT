94d746cbdb123a34b99d6904b7f3360a3676f3d0
vas325 <stanislav.vastyl@vsb.cz>
2022-12-26 19:11:47 +0100

    splashscreen

---
+++ app/src/main/java/com/example/noteapp/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.noteapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import kotlinx.coroutines.delay
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
